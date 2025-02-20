6fe22db6c7279467f37c772e53c2c118db485e11
SaltBr <snowgriff123@gmail.com>
2025-02-20 16:26:31 +0900

    상세 설명 추가

--- src/memberData.js
+++ src/memberData.js
@@ -9,7 +9,7 @@ const members = [
         name: "윤경모",
         id: "2",
         image: "./asset/images/ykm_profile_image.jpg",
-        description: "4567"
+        description: "안녕하세요. 이번 스프링 6기에 참여하게 된 윤경모입니다.\n\n저의 취미로는 코딩하거나 게임하는 것도 좋아하고, 소설도 자주 읽습니다.\n상세페이지에는 이러한 취미들과 관련된 사진을 올려 두었습니다.\n\n잘 부탁드립니다!"
     },
     {
         name: "소진오",
@@ -21,7 +21,7 @@ const members = [
         name: "장희수",
         id: "4",
         image: "./asset/images/jhs_profile_image.jpeg",
-        description: "안녕하세요,\n스프링 6기 장희수입니다.\n\n취미로는 이것저것 만들거나 게임하기를 좋아하며,\n개인 페이지는 제가 좋아하는 것들의 사진으로 채워보았습니다.\n\n잘 부탁드립니다!"
+        description: "안녕하세요, 스프링 6기 장희수입니다.\n\n취미로는 이것저것 만들거나 게임하기를 좋아하며,\n개인 페이지는 제가 좋아하는 것들의 사진으로 채워보았습니다.\n\n잘 부탁드립니다!"
     }
 ]
 

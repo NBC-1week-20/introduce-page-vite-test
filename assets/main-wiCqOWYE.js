78505bcb8239c7268ff7efc8a723d4849b9f2f1f
jin0792 <jin0792@users.noreply.github.com>
2025-02-20 05:37:55 +0000

    deploy: 7de1d1881dd2ebef90caad01b0a62144470a34fe

--- assets/main-wiCqOWYE.js
+++ assets/main-wiCqOWYE.js
@@ -2,8 +2,10 @@ import"./modulepreload-polyfill-B5Qt9EMX.js";import{initializeApp as l}from"http
 
 이것 저것 코드 짜는거 좋아하고 얘기하는거 좋아합니다~ 취미로는 운동 하고 있어요.
 
-잘 부탁드립니다~`},{name:"윤경모",id:"2",image:"https://cdn.pixabay.com/photo/2017/08/07/14/15/woman-2604283_1280.jpg",description:"4567"},{name:"소진오",id:"3",image:"./asset/images/jin_profile_image.jpg",description:`안녕하세요. 스프링 6기 소진오입니다.
-취미는 정말 많은데 그 중에 제일 좋아하는 취미는 축구 보는 거랑 음악 듣기 등등 있습니다.`},{name:"장희수",id:"4",image:"./asset/images/jhs_profile_image.jpeg",description:"899"}],u={apiKey:"AIzaSyBtVPaMcwLkX8OC8fVfAjpZDxfacykBREU",authDomain:"sparta-e02dc.firebaseapp.com",projectId:"sparta-e02dc",storageBucket:"sparta-e02dc.firebasestorage.app",messagingSenderId:"1000457952288",appId:"1:1000457952288:web:472fe9e7f48a94cea37045",measurementId:"G-H41TVN2QFP"},E=l(u),B=d(E);async function I(){let e=document.getElementById("comment-list");e.innerHTML="",(await r(p(g(B,"details"),y("timestamp","desc"),f(3)))).forEach(m=>{let n=m.data(),s=n.author_name,a=n.content,i=n.timestamp?n.timestamp.toDate().toLocaleString():"날짜 없음",c=`<p class="comment"><strong>${s}:</strong> ${a} <br> <small>${i}</small></p>`;e.innerHTML+=c})}window.onload=I;function h(e){return`
+잘 부탁드립니다~`},{name:"윤경모",id:"2",image:"https://cdn.pixabay.com/photo/2017/08/07/14/15/woman-2604283_1280.jpg",description:"4567"},{name:"소진오",id:"3",image:"./asset/images/jin_profile_image.jpg",description:`안녕하세요. 스프링 6기 소진오입니다. 
+저는 배낭여행을 해본 경험이 있는데요. 제 상세페이지에 있는 사진들이 제가 여행 간 나라들 중 대표적인 장소들 몇 개 올려놨습니다. 
+취미는 정말 많은데 그중에 제일 좋아하는 취미는 
+축구 보는 거랑 음악 듣기 등등 있습니다.`},{name:"장희수",id:"4",image:"./asset/images/jhs_profile_image.jpeg",description:"899"}],u={apiKey:"AIzaSyBtVPaMcwLkX8OC8fVfAjpZDxfacykBREU",authDomain:"sparta-e02dc.firebaseapp.com",projectId:"sparta-e02dc",storageBucket:"sparta-e02dc.firebasestorage.app",messagingSenderId:"1000457952288",appId:"1:1000457952288:web:472fe9e7f48a94cea37045",measurementId:"G-H41TVN2QFP"},E=l(u),B=d(E);async function I(){let e=document.getElementById("comment-list");e.innerHTML="",(await r(p(g(B,"details"),y("timestamp","desc"),f(3)))).forEach(m=>{let n=m.data(),s=n.author_name,a=n.content,i=n.timestamp?n.timestamp.toDate().toLocaleString():"날짜 없음",c=`<p class="comment"><strong>${s}:</strong> ${a} <br> <small>${i}</small></p>`;e.innerHTML+=c})}window.onload=I;function h(e){return`
     <div class="member-card">
     <div class="member-wrap">
       <div class="member-left">

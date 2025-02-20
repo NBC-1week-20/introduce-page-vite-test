fd9e83684f5207ad6d7be45e58e29f636c295229
SaltBr <SaltBr@users.noreply.github.com>
2025-02-20 07:27:40 +0000

    deploy: 928b88f60c340d81f5ea4debd7810a4034f00e07

--- assets/main-DiR_aNQd.js
+++ assets/main-DiR_aNQd.js
@@ -2,11 +2,15 @@ import"./modulepreload-polyfill-B5Qt9EMX.js";import{initializeApp as l}from"http
 
 이것 저것 코드 짜는거 좋아하고 얘기하는거 좋아합니다~ 취미로는 운동 하고 있어요.
 
-잘 부탁드립니다~`},{name:"윤경모",id:"2",image:"./asset/images/ykm_profile_image.jpg",description:"4567"},{name:"소진오",id:"3",image:"./asset/images/jin_profile_image.jpg",description:`안녕하세요. 스프링 6기 소진오입니다. 
+잘 부탁드립니다~`},{name:"윤경모",id:"2",image:"./asset/images/ykm_profile_image.jpg",description:`안녕하세요. 이번 스프링 6기에 참여하게 된 윤경모입니다.
+
+저의 취미로는 코딩하거나 게임하는 것도 좋아하고, 소설도 자주 읽습니다.
+상세페이지에는 이러한 취미들과 관련된 사진을 올려 두었습니다.
+
+잘 부탁드립니다!`},{name:"소진오",id:"3",image:"./asset/images/jin_profile_image.jpg",description:`안녕하세요. 스프링 6기 소진오입니다. 
 저는 배낭여행을 해본 경험이 있는데요. 제 상세페이지에 있는 사진들이 제가 여행 간 나라들 중 대표적인 장소들 몇 개 올려놨습니다. 
 취미는 정말 많은데 그중에 제일 좋아하는 취미는 
-축구 보는 거랑 음악 듣기 등등 있습니다.`},{name:"장희수",id:"4",image:"./asset/images/jhs_profile_image.jpeg",description:`안녕하세요,
-스프링 6기 장희수입니다.
+축구 보는 거랑 음악 듣기 등등 있습니다.`},{name:"장희수",id:"4",image:"./asset/images/jhs_profile_image.jpeg",description:`안녕하세요, 스프링 6기 장희수입니다.
 
 취미로는 이것저것 만들거나 게임하기를 좋아하며,
 개인 페이지는 제가 좋아하는 것들의 사진으로 채워보았습니다.
